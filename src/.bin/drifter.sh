de1e67539da7ce2bdff38bc9b0a85756ebd13f01
ITLackey <lackey_i@wustl.edu>
2019-11-13 15:57:22 -0600

    formatting

--- src/.bin/drifter.sh
+++ src/.bin/drifter.sh
@@ -6,36 +6,36 @@ echo "drifter 0.1.0"
 DRIFTER_BIN="$HOME/.bin/drifter"
 DRIFTER_APPS_BIN="$DRIFTER_BIN/apps"
 
-DRIFTER_CURRENT_HUB_BIN="$HOME/.bin/drifter/hubs/$hub/"
-echo $DRIFTER_CURRENT_HUB_BIN
+#DRIFTER_CURRENT_HUB_BIN="$HOME/.bin/drifter/hubs/$hub/"
+#echo "$DRIFTER_CURRENT_HUB_BIN"
 
 case $1 in
-    "init")
-        sh $DRIFTER_BIN/init.sh
+"init")
+    sh "$DRIFTER_BIN/init.sh"
     ;;
 
-    "config")
-        sh $DRIFTER_BIN/config.sh
-    ;;
-    
-    "pull")
-        sh $DRIFTER_CURRENT_HUB_BIN/sync.sh
+"config")
+    sh "$DRIFTER_BIN/config.sh"
     ;;
 
-    "pull")
-        sh $DRIFTER_CURRENT_HUB_BIN/pull.sh
+"pull")
+    sh "$DRIFTER_CURRENT_HUB_BIN/sync.sh"
     ;;
 
-    "sync")
-        sh $DRIFTER_CURRENT_HUB_BIN/pull.sh
+"pull")
+    sh "$DRIFTER_CURRENT_HUB_BIN/pull.sh"
     ;;
 
-    "app")
-        sh $DRIFTER_APPS_BIN/$2.sh
+"sync")
+    sh "$DRIFTER_CURRENT_HUB_BIN/pull.sh"
     ;;
-    
-    *)
-        sh $DRIFTER_BIN/help.sh
+
+"app")
+    sh "$DRIFTER_APPS_BIN/$2.sh"
+    ;;
+
+*)
+    "sh $DRIFTER_BIN/help.sh"
     ;;
 esac
 
