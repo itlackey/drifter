faba88b12c371042dd5a4341c1056c77338267df
ITLackey <lackey_i@wustl.edu>
2019-11-14 09:24:56 -0600

    stubbed notify kde

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-cp -r src/.bin $HOME
-cp -r src/.config $HOME
+#sudo apt install libnotify-bin
+#notify-send -u critical -a drifter "Syncing Error" "Conflicting change"
+cp -r src/.bin "$HOME"
+cp -r src/.config "$HOME"
